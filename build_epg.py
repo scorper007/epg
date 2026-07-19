49a61cfd2eb2af238996184531167623cbc971bc
scorper007 <68543696+scorper007@users.noreply.github.com>
2026-07-19 18:57:47 +0300

    Update build_epg.py

--- build_epg.py
+++ build_epg.py
@@ -4,7 +4,7 @@ import urllib.request
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
-SOURCE = "http://epg.one/epg.xml.gz"
+SOURCE = "http://s06.wsbof.com:8080/xml/b4a9c2c7.gz"
 IDS_FILE = Path("channel_ids.txt")
 OUTPUT = Path("epg.xml")
 
